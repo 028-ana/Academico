88ba5c871fba4ba1903e2f438cc2e9f9a6c612c0
028-ana <anaclara.evg@gmail.com>
2025-05-20 15:45:23 -0300

    Add files via upload

---
+++ Academico/Academico/app/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import *
+from django.contrib import admin
+admin.site.register(AreaSaber)
+admin.site.register(Cidade)
+admin.site.register(Ocupacao)
+admin.site.register(PeriodoCurso)
+admin.site.register(Pessoas)
+admin.site.register(Cursos)
+admin.site.register(Disciplinas)
+admin.site.register(Matricula)
+admin.site.register(Avaliacoes)
+admin.site.register(Frequencia)
+admin.site.register(Turmas)
